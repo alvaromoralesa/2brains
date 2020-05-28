7df7e949bfa65ae6f36f695ae6c626f21a3ff112
alvaro.morales.a <alvaro.morales.a@gmail.com>
2020-05-27 23:03:37 -0400

    formato de card con flexbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,7 +22,7 @@ import { HttpClientModule} from '@angular/common/http';
 import { UserService } from '../app/shared/services/user.service';
 //Flexbox Layout
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { CardComponent } from './components/dashboard/card/card.component';
+//import { CardComponent } from './components/dashboard/card/card.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,7 @@ import { CardComponent } from './components/dashboard/card/card.component';
     ForgotPasswordComponent,
     VerifyEmailComponent,
     SignUpComponent,
-    CardComponent
+    //CardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { AuthService } from "../../shared/services/auth.service";
 import { Router } from "@angular/router";
+import { AuthService } from "../../shared/services/auth.service";
 import { UserService } from '../../shared/services/user.service';
 
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
+  styles: [`
+  :host {
+    display: flexbox;
+    padding: 5px;
+    border: 1px solid black;
+    border-radius: 8px;
+  }
+  `]
 })
 export class DashboardComponent implements OnInit {
 
